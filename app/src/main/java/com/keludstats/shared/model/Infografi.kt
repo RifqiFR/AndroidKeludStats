871deded1ae030e025f4e61f9f1a064ae87b6f00
AanMFA <aanmfa06@gmail.com>
2021-04-21 15:28:20 +0700

    Sambungkan layout dashboard infografis #33

--- app/src/main/java/com/keludstats/shared/model/Infografi.kt
+++ app/src/main/java/com/keludstats/shared/model/Infografi.kt
@@ -1,6 +1,7 @@
 package com.keludstats.shared.model
 
 import com.google.gson.annotations.SerializedName
+import java.text.DateFormatSymbols
 
 class Infografi(
         val id: Int,
@@ -10,4 +11,12 @@ class Infografi(
         val pictureLink: String,
         val caption: String,
         val date: String
-)
+) {
+        // date format is yyyy-mm-dd
+        val dayDate get() = date.substring(date.length - 2, date.length)
+        val monthDate get(): String {
+                val monthDate = date.substring(date.length - 5, date.length - 3)
+
+                return DateFormatSymbols().months[monthDate.toInt()]
+        }
+}
